d2ecb49b9eb0fc485ae0052c15c5ceac8ba83aae
miyass <exe.miyakura0629@gmail.com>
2018-05-27 23:44:38 +0900

    commit

---
+++ spec/autocomplete-kintone-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AutocompleteKintoneView from '../lib/autocomplete-kintone-view';
+
+describe('AutocompleteKintoneView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
