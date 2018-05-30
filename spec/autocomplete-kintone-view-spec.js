e3fcefd340bc65f413d2e6fe15fbe511767057c6
miyass <exe.miyakura0629@gmail.com>
2018-05-30 23:25:35 +0900

    create kintone REST API autocomplete function

--- spec/autocomplete-kintone-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AutocompleteKintoneView from '../lib/autocomplete-kintone-view';
-
-describe('AutocompleteKintoneView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
